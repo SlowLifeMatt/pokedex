6a8689a517680954f19c330ba89efbb6fae285ca
Matthew <subinmatt@gmail.com>
2022-10-17 18:45:07 -0700

    new pokemon route added

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Pokemon = require('./models/pokemon.js');
+const pokemon = require('./models/pokemon.js');
 const app = express();
 const port = 3000;
 const methodOverride = require('method-override');
@@ -7,10 +7,12 @@ const methodOverride = require('method-override');
 
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'));
+
+
 // INDEX
 app.get('/pokemon', (req, res) => {
 res.render('index.ejs', { 
-    data: Pokemon });
+    allPokemon: pokemon });
 });
 // new
 app.get('/pokemon/new', (req, res) => {
@@ -20,45 +22,58 @@ app.get('/pokemon/new', (req, res) => {
 // SHOW
 app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', { 
-        data: Pokemon[req.params.id]
+        allPokemon: pokemon[req.params.id],
+
     });
 });
 
 // create
-app.post('/pokemon', (req,res) => {
+ //post route
+ app.post('/pokemon', (req,res) => {
     console.log(req.body)
-    Pokemon.push(req.body)
+    
+    let stats = {
+        hp: req.body.hp,
+        attack: req.body.attack,
+        defense: req.body.defense,
+        spattack: req.body.spattack,
+        spdefense: req.body.spdefense,
+        speed: req.body.speed
+    }
+    let newPokemon = {
+        name: req.body.name,
+        img: req.body.img,
+        type: req.body.types,
+        stats: stats,
+
+    }
+    pokemon.push(newPokemon)
+    // res.send('Data have been received')
+  
     res.redirect('/pokemon')
-})
+  })
+
 // delete 
 app.delete('/pokemon/:id', (req,res) => {
     console.log("delete route")
-    Pokemon.splice(req.params.id,1);
+    pokemon.splice(req.params.id,1);
     res.redirect('/pokemon')
 })
 
-// edit
+//edit
 app.get('/pokemon/:id/edit', (req, res) => {
     res.render('edit.ejs',  {
-        data: Pokemon[req.params.id],
+        allPokemon: pokemon[req.params.id],
         id: req.params.id, 
       }
     );
   });
  // update
  app.put('/pokemon/:id', (req,res) => {
-    Pokemon[req.params.id] = req.body
+    pokemon[req.params.id] = req.body
     res.redirect('/pokemon')
  }) 
- //post route
-app.post('/pokemon', (req,res) => {
-    console.log(req.body)
-  
-    Pokemon.push(req.body)
-    // res.send('Data have been received')
-  
-    res.redirect('/pokemon')
-  })
+
 
 // listener
 app.listen(port, () => {
