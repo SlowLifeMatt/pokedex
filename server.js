4bf8529e5a2d4415daf4aba4f2fda5d936fdb730
Matthew <subinmatt@gmail.com>
2022-10-17 17:06:04 -0700

    new create page, WIP

--- server.js
+++ server.js
@@ -8,22 +8,65 @@ const methodOverride = require('method-override');
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'));
 // INDEX
-app.get('/', (req, res) => {
+app.get('/pokemon', (req, res) => {
 res.render('index.ejs', { 
     data: Pokemon });
 });
-
+// new
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs')
+})
 
 // SHOW
-app.get('/:id', (req, res) => {
-res.render('show.ejs', { 
-    data: Pokemon[req.params.id]
-});
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', { 
+        data: Pokemon[req.params.id]
+    });
 });
 
+// create
+app.post('/pokemon', (req,res) => {
+    console.log(req.body)
+    Pokemon.push(req.body)
+    res.redirect('/pokemon')
+})
+// delete 
+app.delete('/pokemon/:id', (req,res) => {
+    console.log("delete route")
+    Pokemon.splice(req.params.id,1);
+    res.redirect('/pokemon')
+})
 
+// edit
+app.get('/pokemon/:id/edit', (req, res) => {
+    res.render('edit.ejs',  {
+        data: Pokemon[req.params.id],
+        id: req.params.id, 
+      }
+    );
+  });
+ // update
+ app.put('/pokemon/:id', (req,res) => {
+    Pokemon[req.params.id] = req.body
+    res.redirect('/pokemon')
+ }) 
+ //post route
+app.post('/pokemon', (req,res) => {
+    console.log(req.body)
+  
+    Pokemon.push(req.body)
+    // res.send('Data have been received')
+  
+    res.redirect('/pokemon')
+  })
 
 // listener
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
   });
+
+
+
+
+
+
